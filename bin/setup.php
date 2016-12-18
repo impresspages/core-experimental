534b87466d316b276a6ef9c71b8181edb820e8c2
Mangirdas Skripka <mangirdas.skripka@gmail.com>
2016-12-18 22:51:01 +0200

    starter-pack

--- bin/setup.php
+++ bin/setup.php
@@ -21,3 +21,26 @@ foreach ($assetPaths as $assetPath) {
     `mkdir -p public/Ip/Internal/$module/`;
     `cp -rf $assetPath public/Ip/Internal/$module`;
 }
+
+if (!is_dir('public/Plugin')) {
+    `cp -rf vendor/impresspages/core/start-pack/Plugin public/`;
+}
+
+if (!is_dir('public/Theme')) {
+    `cp -rf vendor/impresspages/core/start-pack/Theme public/`;
+}
+
+if (!is_dir('public/file')) {
+    `cp -rf vendor/impresspages/core/start-pack/file public/`;
+}
+
+//user could have removed admin.php or favicon.ico.
+if (
+    !is_file('public/admin.php') &&
+    !is_file('public/favicon.ico') &&
+    !is_file('public/index.php')
+) {
+    `cp -rf vendor/impresspages/core/start-pack/admin.php public/`;
+    `cp -rf vendor/impresspages/core/start-pack/favicon.ico public/`;
+    `cp -rf vendor/impresspages/core/start-pack/index.php public/`;
+}
